e636fe1cc5fd4ba3861128700ac0830e653ba967
jion-kozono <zeon66190329@gmail.com>
2019-10-22 19:55:46 +0900

    First commit 課題：得点を表示しようまで

---
+++ Assets/Lesson5 2/BallController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    public ScoreText scoreText;　//外部のScoteTextオブジェクトを見えるよう定義
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            scoreText.GetComponent<ScoreText>().score += 5;
+        }
+        else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            scoreText.GetComponent<ScoreText>().score += 30;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag" || collision.gameObject.tag == "LargeCloudTag")
+        {
+            scoreText.GetComponent<ScoreText>().score += 10;
+        }
+    }
+}
---
+++ Assets/Lesson5 2/ScoreText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    //点数を格納する変数
+    public int score = 0;
+
+    //ポイントを表示するテキスト
+    private GameObject PointText;
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中の10pTextオブジェクトを取得
+        this.PointText = GameObject.Find("PointText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.PointText.GetComponent<Text>().text = "点数" + this.score + "点";
+    }
+}
